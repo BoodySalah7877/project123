5c53350d735a5f98d0a88bc9a05e803edd1e2965
BoodySalah7877 <77878350+BoodySalah7877@users.noreply.github.com>
2022-12-21 20:35:12 +0200

    Add files via upload

---
+++ مشروع 1 naz/مشروع 1 naz/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int start = int.Parse(Console.ReadLine());
+
+int end = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = start; i <= end; i++)
+{
+    for (int x = 1; x <= i; x++)
+    {
+        if (i % x == 0)
+            sum++;
+    }
+    if (sum == 2)
+    {
+        Console.WriteLine(i);
+        sum = 0;
+    }
+    else
+    {
+        sum = 0;
+        
+    }
+}
+
---
+++ مشروع 2 naz/مشروع 2 naz/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello,MR NAZROOT");
+
+Console.Write("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+{
+
+        int z, b, sum;
+
+        int m1n, m2x;
+
+Console.Write("\n\n");
+
+Console.Write("Find perfect numbers within a given number of range:\n");
+
+Console.Write("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+
+Console.Write("\n\n");
+
+Console.Write("Input the starting range or 1number : ");
+
+m1n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the ending range of 2number : ");
+
+m2x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("The Perfect numbers within the given range : ");
+
+for (z = m1n; z <= m2x; z++)
+{
+    b = 1;
+    sum = 0;
+    while (b < z)
+    {
+        if (z % b == 0)
+            sum = sum + b;
+        b++;
+    }
+    if (sum == z)
+        Console.Write("{0} ", z);
+}
+
+Console.Write("\n");
+
+ }
+    
